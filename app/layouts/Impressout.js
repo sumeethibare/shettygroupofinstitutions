992d43961ecd1b39645a8cc2c36fe25d0a12a560
sumeet hibare <theforeverfella@gmail.com>
2024-05-18 23:54:00 +0530

    updates

--- app/layouts/Impressout.js
+++ app/layouts/Impressout.js
@@ -7,7 +7,7 @@ function Impressout() {
                 <h1 className='lg:text-8xl text-6xl font-serif font-medium italic text-blue-950 lg:p-10'>
                     creativity meets talent.
                 </h1>
-                <a className='lg:p-20 lg:px-40 bg-black rounded-full p-16 px-32'></a>
+                <a className='lg:p-20 lg:px-40 border-8 border-black rounded-full p-16 px-32'></a>
             </div>
         </>
     )
--- app/univcom/Fluidbar.js
+++ app/univcom/Fluidbar.js
@@ -18,12 +18,12 @@ const Fluidbar = () => {
             {/* Navbar */}
             <div id="myNav" className="overlay no-scrollbar">
                 <div className="overlayContent">
-                    <div className='flex flex-col max-h-screen no-scrollbar bg-white' onClick={toggleNav}>
+                    <div className='flex flex-col max-h-screen no-scrollbar bg-zinc-950' onClick={toggleNav}>
                         {/* Top content */}
                         <div className='overflow-auto lg:overflow-hidden no-scrollbar'>
-                            <div className='flex lg:flex-row flex-col border-t-2 divide-x-2 divide-zinc-400 h-screen'>
+                            <div className='flex lg:flex-row flex-col border-t-2 divide-x-2 divide-zinc-800 h-screen'>
                                 <div className='flex flex-col lg:space-y-5 space-y-2 px-6 lg:p-14 p-8 h-max lg:w-3/12'>
-                                    <p className='text-xs lg:pb-10 tracking-widest font-bold text-zinc-950'>SECTIONS</p>
+                                    <p className='text-xs lg:pb-10 tracking-widest font-bold text-zinc-300'>SECTIONS</p>
                                     <ps>About Us</ps>
                                     <ps>Management</ps>
                                     <ps>News</ps>
@@ -32,16 +32,16 @@ const Fluidbar = () => {
                                     <ps>Compass</ps>
                                     <ps>Portals</ps>
                                 </div>
-                                <div className='flex flex-col lg:flex-row divide-x-2 lg:w-11/12 divide-zinc-400'>
+                                <div className='flex flex-col lg:flex-row divide-x-2 lg:w-11/12 divide-zinc-800'>
                                     <div className='flex flex-col lg:space-y-5 space-y-2 px-6 lg:w-4/12 lg:p-14 p-8'>
-                                        <p className='text-xs lg:pb-10 tracking-widest font-bold text-zinc-950'>QUICK LINKS</p>
+                                        <p className='text-xs lg:pb-10 tracking-widest font-bold text-zinc-300'>QUICK LINKS</p>
                                         <ps>Student Center</ps>
                                         <ps>Enrollment</ps>
                                         <ps>Contribute</ps>
                                         <ps>Contact</ps>
                                     </div>
                                     <div className='flex flex-col lg:space-y-5 space-y-2 px-6 lg:w-8/12 lg:p-14 p-8'>
-                                        <p className='text-xs lg:pb-10 tracking-widest font-bold text-zinc-950'>SUBSIDERIES</p>
+                                        <p className='text-xs lg:pb-10 tracking-widest font-bold text-zinc-300'>SUBSIDERIES</p>
                                         <ps>Homoeopathy</ps>
                                         <ps>Ayurveda</ps>
                                         <ps>Paramedical</ps>
@@ -53,7 +53,7 @@ const Fluidbar = () => {
                             </div>
                         </div>
                         {/* Bottom content */}
-                        <div className='h-24 w-full lg:flex hidden lg:flex-row flex-col lg:justify-between border-t-2 border-zinc-400 divide-x-2 lg:px-5 divide-zinc-400'>
+                        <div className='h-24 w-full lg:flex hidden lg:flex-row flex-col lg:justify-between border-t-2 border-zinc-800 divide-x-2 lg:px-5 divide-zinc-800 text-zinc-300'>
                             <div className='lg:w-3/12 lg:pr-14 py-10 flex justify-evenly'>
                                 <FaFacebookF size={20} />
                                 <FaInstagram size={20} />
@@ -72,9 +72,9 @@ const Fluidbar = () => {
 
             {/* Navbar access button */}
 
-            <div className='float float-right right-10 top-10 z-50 sticky'>
+            <div className='float float-right right-10 top-10 z-50 sticky bg-white rounded-full'>
                 <button onClick={toggleNav} className="btn btn-ghost btn-lg btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="black" className="w-8 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
                         {navOpen ?
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" /> :
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
